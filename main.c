8fe49b66bc77d62228db30c68c6562b42754c270
Mouhamed27-99 <medsoseck@gmail.com>
2022-01-01 14:21:18 +0100

    feat: initial commit et fonction utlitaires

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+
+
+
+
+
+int main() {
+    
+    FILE *out = fopen("sortie1.txt", "w");
+    
+    uint8_t board_state[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+    
+}
